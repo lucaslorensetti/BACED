954c6aff14125f5ef28c79adb42320ed66659247
Lucas Lorensetti <lucas.l@easycomtec.com>
2018-12-30 16:54:19 -0200

    Fixed packages.

--- src/BACED.WebApi/Startup.cs
+++ src/BACED.WebApi/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,14 +8,14 @@ namespace BACED.WebApi {
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
-			services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+			services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Latest);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
